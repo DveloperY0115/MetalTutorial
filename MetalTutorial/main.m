71ec1a8f80927e8dd747b54dd0f7228516d6c6b8
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2021-10-31 22:11:40 +0900

    Initial Commit

---
+++ MetalTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MetalTutorial
+//
+//  Created by 유승우 on 2021/10/31.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
